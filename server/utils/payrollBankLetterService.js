35b9fe181ba99e90e880b71a61b65f50a46ad800
Shehroze <shehroze@Shehrozes-MacBook-Pro.local>
2026-07-08 16:00:46 +0500

    Fix bank letter to use account description instead of ledger code for bank account

--- server/utils/payrollBankLetterService.js
+++ server/utils/payrollBankLetterService.js
@@ -77,7 +77,7 @@ const buildLetterPayload = async (app) => {
       companyBank = {
         ...(companyBank || {}),
         bankName: voucherAccount.name || companyBank?.bankName || '',
-        accountNumber: companyBank?.accountNumber || voucherAccount.accountNumber || ''
+        accountNumber: companyBank?.accountNumber || voucherAccount.description || ''
       };
     }
   }
